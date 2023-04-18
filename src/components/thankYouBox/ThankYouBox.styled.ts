f881447fd3fb925c61a4c2a756c0bb506b73fb0d
Nico Rithner <nicorithner@gmail.com>
2023-04-18 14:32:03 -0600

    Styled Thank You Box using provided figma file

---
+++ src/components/thankYouBox/ThankYouBox.styled.ts
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+import { GRADIENTS, NEUTRAL, PRIMARY } from "../../constants";
+
+export const TYBoxWrapper = styled.section`
+  border: 1px solid black;
+  background: ${GRADIENTS.blackPearlGradient};
+  height: 416px;
+  width: 412px;
+  border-radius: 30px;
+  padding: 45px;
+  padding-left: 32px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+export const TYBoxImg = styled.img`
+  width: 162px;
+  height: 108px;
+`;
+export const TYBoxResult = styled.div`
+  background: ${PRIMARY.darkBlue};
+  width: 193px;
+  height: 32px;
+  border-radius: 22.5px;
+`;
+export const TYBoxResultText = styled.p`
+  color: ${PRIMARY.orange};
+  line-height: ${32 / 16}rem;
+  font-family: "Overpass";
+  font-style: normal;
+  font-weight: 400;
+  font-size: ${16/16}rem;
+  text-align: center;
+`;
+export const TYBoxTitle = styled.h1`
+  color: ${NEUTRAL.white};
+  font-family: "Overpass";
+  font-style: normal;
+  font-weight: 700;
+  font-size: ${28 / 16}rem;
+  line-height: ${35 / 16}rem;
+`;
+export const TYBoxText = styled.p`
+  color: ${NEUTRAL.lightGray};
+  font-family: "Overpass";
+  font-style: normal;
+  font-weight: 400;
+  font-size: ${16 / 16}rem;
+  line-height: ${24 / 16}rem;
+  text-align: center;
+`;
