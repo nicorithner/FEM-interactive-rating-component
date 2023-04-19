5fe03cee9c051621653ec5ff27d095d9b763ccd2
Nico Rithner <nicorithner@gmail.com>
2023-04-19 13:32:19 -0600

    Add function to handle box visibility when submit

--- src/App.tsx
+++ src/App.tsx
@@ -4,12 +4,20 @@ import { RatingBox } from "./components/ratingBox/RatingBox";
 import { ThankYouBox } from "./components/thankYouBox/ThankYouBox";
 
 function App() {
-  const [tYBoxDisplay, setTYBoxDisplay] = useState(false)
+  const [tYBoxDisplay, setTYBoxDisplay] = useState(false);
+  const [ratingBoxDisplay, setRatingBoxDisplay] = useState(true);
+
+  const handleSubmit = (
+    event: React.MouseEvent<HTMLButtonElement, MouseEvent>
+  ) => {
+    setRatingBoxDisplay(false);
+    setTYBoxDisplay(true);
+  };
 
   return (
     <AppWrapper>
-      <RatingBox />
-      <ThankYouBox display={tYBoxDisplay}/>
+      <RatingBox display={ratingBoxDisplay} handleSubmit={handleSubmit} />
+      <ThankYouBox display={tYBoxDisplay} />
     </AppWrapper>
   );
 }
