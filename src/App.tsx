bc1fa7ebcad5cb080919ae11e8f626a116cfa000
Nico Rithner <nicorithner@gmail.com>
2023-04-19 11:11:57 -0600

    Add state for thakYouBox display status

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
+import { useState } from "react";
 import { AppWrapper } from "./components/App.styled";
 import { RatingBox } from "./components/ratingBox/RatingBox";
 import { ThankYouBox } from "./components/thankYouBox/ThankYouBox";
 
 function App() {
+  const [tYBoxDisplay, setTYBoxDisplay] = useState(false)
+
   return (
     <AppWrapper>
       <RatingBox />
-      <ThankYouBox />
+      <ThankYouBox display={tYBoxDisplay}/>
     </AppWrapper>
   );
 }
